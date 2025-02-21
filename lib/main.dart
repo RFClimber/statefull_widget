16a6d02f24d2c93b6d3b0aa7b3e762cd7f146a45
RFClimber <choiwoolim98@gmail.com>
2025-02-21 13:54:31 +0900

    stateful_widget_practice

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,72 @@ class MainApp extends StatefulWidget {
 
 // UI와 Data를 저장함
 class _MainAppState extends State<MainApp> {
-  List<int> numbers = [];
+  bool showTitle = false;
 
-  void onClicked() {
+  void toggleTitle() {
     setState(() {
-      numbers.add(numbers.length);
+      showTitle = !showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(color: Colors.red),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Click Count", style: TextStyle(fontSize: 30)),
-              for (var n in numbers) Text("$n"),
+              showTitle ? MyLargeTitle() : Text("Nothing"),
               IconButton(
-                onPressed: onClicked,
-                icon: Icon(Icons.add_box_rounded),
+                onPressed: toggleTitle,
+                icon: Icon(Icons.change_circle),
                 iconSize: 40,
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  // 부모 요소에 의존하는 데이터를 초기화 해야하는 경우
+  @override
+  void initState() {
+    super.initState();
+    print("initState");
+  }
+
+  // 위젯이 스크린에서 제거될 때 호출되는 메소드
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Text(
+      "My Large Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
