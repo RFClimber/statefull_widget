aa26f3a10cb0e8cba14ff07ac58af8806b6d3b41
RFClimber <choiwoolim98@gmail.com>
2025-02-21 13:00:46 +0900

    set_state

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,41 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+// UI와 Data를 저장함
+class _MainAppState extends State<MainApp> {
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Click Count", style: TextStyle(fontSize: 30)),
+              for (var n in numbers) Text("$n"),
+              IconButton(
+                onPressed: onClicked,
+                icon: Icon(Icons.add_box_rounded),
+                iconSize: 40,
+              ),
+            ],
+          ),
         ),
       ),
     );
